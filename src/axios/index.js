48284d9c8561977499353527dda3408e23b40f32
ziqian9206 <ziqian9206@126.com>
2019-03-03 18:29:59 +0800

    feat:axios改端口

--- src/axios/index.js
+++ src/axios/index.js
@@ -12,7 +12,7 @@ http.post = (url,data,callback)=>{
         axios({
             method:"POST",
             headers:{'Content-type':'application/json',},
-            url:url,
+            url:`http://127.0.0.1:7001/${url}`,
             data:data,
         }).then((res)=>{
             const response = res.data
@@ -32,7 +32,7 @@ http.get = (url,data)=>{
         axios({
             method:"GET",
             headers:{'Content-type':'application/json',},
-            url:url,
+            url:`http://127.0.0.1:7001/${url}`,
             params:data,
         }).then((res)=>{
             const response = res.data
@@ -53,66 +53,4 @@ export default http
 
 
 
-
-// export function ajax_post(url,data,that,callback){
-//     axios({
-//         method:"POST",
-//         headers:{'Content-type':'application/json',},
-//         url:url,
-//         data:data,
-//     }).then(function(res){
-//         console.log(url+'\tPost请求到:');
-//         console.log(res);
-//         if(res.error===0){
-//             callback(that,res);
-//         }else{
-//             message.error(res.msg);
-//         }
-//         //ajax_get('/manage/getinfo',this);
-//     }).catch(function(error){
-//         message.error(String(error || '网络错误'))
-//         console.log(error);
-//     });
-// }
-// export function ajax_get(url,params,that,callback=()=>{}){
-//     axios({
-//         method:"GET",
-//         headers:{'Content-type':'application/json',},
-//         url:url,
-//         params:params,
-//         withCredentials:true
-//     }).then(function(res){
-//         console.log(url+'\tGet请求到:')
-//         console.log(res);
-//         let response = res.data
-//         if(response.error===0){
-//             callback(that,response);
-//         }
-//         else{
-//             message.error(response.msg);
-//         }
-//     }).catch(function(error){
-//         message.error(String(error || '请求失败'))
-//         console.log(error);
-//     });
-// }
-// export function ajax_post_params(url,data,that,callback=()=>{}){
-//     axios({
-//         method: 'post',
-//         url: url,
-//         headers: {
-//             'Content-type': 'application/x-www-form-urlencoded',
-//         },
-//         params:data,
-//     })
-//     .then(function(res){
-//         console.log(url+'\tPost请求到:');
-//         console.log(res);
-//         callback(that,res);
-//         //ajax_get('/manage/getinfo',this);
-//     }).catch(function(error){
-//         message.error(String(error || '请求失败'))
-//         console.log(error);
-//     });
-// }
 
